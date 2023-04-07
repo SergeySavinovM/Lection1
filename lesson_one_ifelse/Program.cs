456ffabdd50e6d9b2bf1da81125eae1b78c8e31b
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-07 12:21:15 +0300

    добавили приветсвтвие по имсени

---
+++ lesson_one_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower == "сергей")
+{
+    Console.WriteLine("Саламалейкум работяга!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
