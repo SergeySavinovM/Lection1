25fffa47da240a2708634057d9e4de7e598fe6e5
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-07 12:11:54 +0300

    добавили рандом

--- lesson_one_sum/Program.cs
+++ lesson_one_sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numOne = 3;
-int numTwo = 4;
+﻿int numOne = new Random().Next(1, 10);
+Console.WriteLine(numOne);
+int numTwo = new Random().Next(1, 10);
+Console.WriteLine(numTwo);
 Console.WriteLine(numOne + numTwo);
